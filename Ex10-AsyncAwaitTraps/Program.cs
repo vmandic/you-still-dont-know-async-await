b1b6b436d54120f7f40bf94d16b5227dc68e78ab
Vedran Mandić <mandic.vedra@gmail.com>
2019-04-01 14:27:52 +0200

    add empty Ex9 and Ex10

---
+++ Ex10-AsyncAwaitTraps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex10_AsyncAwaitTraps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
