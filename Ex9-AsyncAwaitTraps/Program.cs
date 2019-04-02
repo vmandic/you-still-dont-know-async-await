5a0263706d863d1d601d5fb52a76ae589d3e211a
Vedran Mandić <mandic.vedra@gmail.com>
2019-04-02 11:17:09 +0200

    finished

--- Ex9-AsyncAwaitTraps/Program.cs
+++ Ex9-AsyncAwaitTraps/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using static System.Console;
 
@@ -8,13 +10,18 @@ namespace Ex9_AsyncAwaitTraps
   {
     async static Task Main(string[] args)
     {
+      WriteLine("[START] Starting app...");
       var result = await AsyncOpBad1();
-      WriteLine($"Some async nesting result: {result}");
 
+      #region Good usage example
+      // var result = await AsyncGoodOp1();
+      #endregion
+
+      WriteLine($"Some async nesting result: {result}");
       WriteLine("[END] The end!");
     }
 
-    #region STEP 1. unnecessary async nesting, try-catch without await
+    #region TRAP 1. unnecessary async nesting, try-catch without await
     async static Task<int> AsyncOpBad1()
     {
       await Task.Delay(1000);
@@ -23,44 +30,91 @@ namespace Ex9_AsyncAwaitTraps
 
     async static Task<int> AsyncOpBad2()
     {
-      return await AsyncOpBad3();
+        return await AsyncOpBad3();
     }
 
-    async static Task AsyncOpBad3()
+    async static Task<int> AsyncOpBad3()
     {
       return await Task.Run(() => 42);
     }
 
-    #region TRAP 1.sln
-    async static Task<int> AsyncOpGood1()
+    #region sln
+    static Task<int> AsyncOpGood1()
     {
-      await Task.Delay(1000);
       return AsyncOpGood2();
     }
 
     static Task<int> AsyncOpGood2()
     {
-      return AsyncOpGood3();
+      try
+      {
+        return AsyncOpGood3();
+      }
+      catch (Exception ex)
+      {
+        WriteLine($"Whoopsieee! {ex.Message}");
+      }
+
+      return Task.FromResult(0);
     }
 
-    async static Task AsyncOpGood3()
+    static Task<int> AsyncOpGood3()
     {
+      #region TRAP 1.1.
+      // throw new Exception("Err!");
+      #endregion
       return Task.Run(() => 42);
     }
     #endregion
     #endregion
 
     #region TRAP 2. hidden danger of ForEach extension
-    // TODO
+
+    async static Task WriteData()
+    {
+      var dataPoints = new List<byte> { 0xAF, 0x4E, 0xC8, 0xF1 };
+
+      // is this alright?
+      dataPoints.ForEach(data => WriteToDisk(data));
+
+      #region is this alrightier?
+      // dataPoints.ForEach(WriteToDisk);
+      #endregion
+
+      #region this must be it? :-)
+      // dataPoints.ForEach(async data => await WriteToDisk(data));
+      #endregion
+
+      #region or is it this POSS
+      // ... just don't do the above, let Eric explain it: https://blogs.msdn.microsoft.com/ericlippert/2009/05/18/foreach-vs-foreach/
+      foreach (var data in dataPoints) await WriteToDisk(data);
+      #endregion
+
+      async Task WriteToDisk(byte data)
+      {
+        await Task.Delay(2000);
+        WriteLine($"Successfully written to disk: '{data}'");
+      }
+    }
     #endregion
 
     #region TRAP 3. sync-over-async with Streams
-    // TODO
+    async static Task WriteStream(Stream httpContextBodyStream)
+    {
+      using(var streamWriter = new StreamWriter(httpContextBodyStream))
+      {
+        await streamWriter.WriteAsync("Hi folks! That's it for today.");
+
+        #region open me :-) and do not forget about me
+        await streamWriter.FlushAsync();
+        #endregion
+      }
+    }
     #endregion
 
-    #region TRAP 4. overparallelization
-    // TODO
-    #endregion
-
+    // TRAP 4. Careful with someTask.ConfigureAwait(continueOnCapturedContext: false);
+    // Always write ConfigureAwait(false) all the way until you return from your library code.
+    // You do not need to use it anymore in ASP.NET Core.
+    // ref: https://blog.stephencleary.com/2017/03/aspnetcore-synchronization-context.html
   }
 }
